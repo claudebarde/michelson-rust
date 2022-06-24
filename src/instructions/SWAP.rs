f0301ccacd41378152eea7600db760802953606e
claudebarde <lotolosenc.oc@gmail.com>
2022-06-24 09:33:59 +0100

    First test for SWAP

--- src/instructions/SWAP.rs
+++ src/instructions/SWAP.rs
@@ -19,3 +19,45 @@ pub fn run(
 
     Ok((new_stack, stack_snapshots))
 }
+
+/**
+ * TESTS
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::stack::StackElement;
+    use crate::m_types::MValue;
+    use crate::instructions::RunOptionsContext;
+
+    // PASSING
+    #[test]
+    fn swap_success() {
+        let initial_stack: Stack = vec![
+            StackElement::new(MValue::String(String::from("test")), Instruction::INIT),
+            StackElement::new(MValue::Int(6), Instruction::INIT),
+            StackElement::new(MValue::Mutez(6_000_000), Instruction::INIT),
+        ];
+        let stack_snapshots = vec![];
+        let options = RunOptions {
+            context: RunOptionsContext {
+                amount: 0,
+                sender: String::from("test_sender"),
+                source: String::from("test_source"),
+            },
+            pos: 0,
+        };
+
+        assert!(initial_stack.len() == 3);
+
+        match run(initial_stack, &options, stack_snapshots) {
+            Ok((new_stack, _)) => {
+                assert!(new_stack.len() == 3);
+                assert!(new_stack[0].value == MValue::Int(6));
+                assert!(new_stack[1].value == MValue::String(String::from("test")));
+            }
+            Err(_) => assert!(false),
+        }
+    }
+}
