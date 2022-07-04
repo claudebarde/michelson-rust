efafd1297535a3ac4b26a2aef319e8ee35f87924
claudebarde <lotolosenc.oc@gmail.com>
2022-07-04 08:42:37 +0100

    Successfully ran contract with AMOUNT, more tests needed

--- src/instructions/IF.rs
+++ src/instructions/IF.rs
@@ -33,33 +33,17 @@ pub fn run(
             }
         }
     };
-    // unwraps the value
-    let (or_element, stack) = stack.remove_at(options.pos);
+    // unwraps the value and removes the element from the stack
+    let (or_element, new_stack) = stack.remove_at(options.pos);
     // processes the stack element value
     match or_element.value {
         MValue::Bool(val) => {
-            // // gets the corresponding arguments and m_value
-            // let (new_args, m_val): (String, MValue) = match *box_.value {
-            //     Or::Left(left_val) => (args[0].to_string(), left_val),
-            //     Or::Right(right_val) => (args[1].to_string(), right_val),
-            // };
-            // // Pushes unwrapped value to the stack
-            // let mut stack_head = vec![StackElement::new(m_val, Instruction::IF_LEFT)];
-            // let mut stack_tail = stack.clone();
-            // stack_head.append(&mut stack_tail);
-            // // updates the stack snapshots
-            // stack_snapshots.push(stack_head.clone());
-            // // runs the code inside the argument
-            // parser::run(new_args.as_str(), stack_head, stack_snapshots)
-
             // gets the corresponding arguments
             let new_args = if val == true {
                 args[0].to_string()
             } else {
                 args[1].to_string()
             };
-            // removes the boolean value from the stack
-            let (_, new_stack) = stack.remove_at(options.pos);
             // updates the stack snapshots
             stack_snapshots.push(new_stack.clone());
             // runs the code inside the argument
