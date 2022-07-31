733be59749c715a496d2abecb0926200f9fbfd8e
claudebarde <lotolosenc.oc@gmail.com>
2022-07-31 19:32:11 +0100

    Started work on MEM instruction (WIP)
    Changed committer

--- src/instructions/MEM.rs
+++ src/instructions/MEM.rs
@@ -11,9 +11,10 @@ pub fn run(
 ) -> Result<(Stack, StackSnapshots), String> {
     // checks the stack
     stack.check_depth(options.pos + 2, Instruction::MEM)?;
-    let el_to_find = &stack[options.pos + 1].value;
+
+    let el_to_find = &stack[options.pos].value;
     // second element on the stack must be a set, a map or a bigmap
-    let result = match &stack[options.pos].value {
+    let result = match &stack[options.pos + 1].value {
         MValue::Set(set) => {
             // finds if value is in the set
             match set.value.iter().find(|&x| x == el_to_find) {
@@ -41,4 +42,205 @@ pub fn run(
     stack_snapshots.push(new_stack.clone());
 
     Ok((new_stack, stack_snapshots))
+}
+
+/**
+ * TESTS
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::instructions::RunOptionsContext;
+    use crate::m_types::{MType, PairValue};
+
+    // PASSING
+    #[test]
+    fn mem_set_success() {
+        // if the element is in the set
+        let initial_stack: Stack = vec![
+            StackElement::new(MValue::Nat(8), Instruction::INIT),
+            StackElement::new(
+                MValue::new_set(
+                    vec![MValue::Nat(7), MValue::Nat(6), MValue::Nat(8)],
+                    MType::Nat,
+                ),
+                Instruction::INIT,
+            ),
+            StackElement::new(MValue::Nat(5), Instruction::INIT),
+            StackElement::new(MValue::Int(6), Instruction::INIT),
+        ];
+        let stack_snapshots = vec![];
+        let options = RunOptions {
+            context: RunOptionsContext {
+                amount: 0,
+                sender: String::from("test_sender"),
+                source: String::from("test_source"),
+                self_address: String::from("KT1L7GvUxZH5tfa6cgZKnH6vpp2uVxnFVHKu"),
+                balance: 50_000_000,
+                level: 11,
+            },
+            pos: 0,
+        };
+
+        assert!(initial_stack.len() == 4);
+
+        match run(initial_stack, &options, stack_snapshots) {
+            Err(_) => assert!(false),
+            Ok((stack, _)) => {
+                assert!(stack.len() == 3);
+                assert_eq!(stack[0].value, MValue::Bool(true));
+                assert_eq!(stack[0].instruction, Instruction::MEM);
+            }
+        }
+
+        // if the element is not in the set
+        let initial_stack: Stack = vec![
+            StackElement::new(MValue::Nat(9), Instruction::INIT),
+            StackElement::new(
+                MValue::new_set(
+                    vec![MValue::Nat(7), MValue::Nat(6), MValue::Nat(8)],
+                    MType::Nat,
+                ),
+                Instruction::INIT,
+            ),
+            StackElement::new(MValue::Nat(5), Instruction::INIT),
+            StackElement::new(MValue::Int(6), Instruction::INIT),
+        ];
+        let stack_snapshots = vec![];
+
+        assert!(initial_stack.len() == 4);
+
+        match run(initial_stack, &options, stack_snapshots) {
+            Err(_) => assert!(false),
+            Ok((stack, _)) => {
+                assert!(stack.len() == 3);
+                assert_eq!(stack[0].value, MValue::Bool(false));
+                assert_eq!(stack[0].instruction, Instruction::MEM);
+            }
+        }
+    }
+
+    #[test]
+    fn mem_map_success() {
+        // if the element is in the set
+        let initial_stack: Stack = vec![
+            StackElement::new(MValue::Nat(8), Instruction::INIT),
+            StackElement::new(
+                MValue::new_map(
+                    MType::Nat,
+                    MType::Int,
+                    vec![
+                        (MValue::Nat(7), MValue::Int(7)),
+                        (MValue::Nat(6), MValue::Int(6)),
+                        (MValue::Nat(8), MValue::Int(8)),
+                    ],
+                ),
+                Instruction::INIT,
+            ),
+            StackElement::new(MValue::Nat(5), Instruction::INIT),
+            StackElement::new(MValue::Int(6), Instruction::INIT),
+        ];
+        let stack_snapshots = vec![];
+        let options = RunOptions {
+            context: RunOptionsContext {
+                amount: 0,
+                sender: String::from("test_sender"),
+                source: String::from("test_source"),
+                self_address: String::from("KT1L7GvUxZH5tfa6cgZKnH6vpp2uVxnFVHKu"),
+                balance: 50_000_000,
+                level: 11,
+            },
+            pos: 0,
+        };
+
+        assert!(initial_stack.len() == 4);
+
+        match run(initial_stack, &options, stack_snapshots) {
+            Err(_) => assert!(false),
+            Ok((stack, _)) => {
+                assert!(stack.len() == 3);
+                assert_eq!(stack[0].value, MValue::Bool(true));
+                assert_eq!(stack[0].instruction, Instruction::MEM);
+            }
+        }
+
+        // if the element is not in the set
+        let initial_stack: Stack = vec![
+            StackElement::new(MValue::Nat(9), Instruction::INIT),
+            StackElement::new(
+                MValue::new_map(
+                    MType::Nat,
+                    MType::Int,
+                    vec![
+                        (MValue::Nat(7), MValue::Int(7)),
+                        (MValue::Nat(6), MValue::Int(6)),
+                        (MValue::Nat(8), MValue::Int(8)),
+                    ],
+                ),
+                Instruction::INIT,
+            ),
+            StackElement::new(MValue::Nat(5), Instruction::INIT),
+            StackElement::new(MValue::Int(6), Instruction::INIT),
+        ];
+        let stack_snapshots = vec![];
+
+        assert!(initial_stack.len() == 4);
+
+        match run(initial_stack, &options, stack_snapshots) {
+            Err(_) => assert!(false),
+            Ok((stack, _)) => {
+                assert!(stack.len() == 3);
+                assert_eq!(stack[0].value, MValue::Bool(false));
+                assert_eq!(stack[0].instruction, Instruction::MEM);
+            }
+        }
+    }
+
+    #[test]
+    fn mem_set_complex_type() {
+        // if the element is in the set
+        let initial_stack: Stack = vec![
+            StackElement::new(
+                PairValue::new(MValue::Nat(7), MValue::Int(11)),
+                Instruction::INIT,
+            ),
+            StackElement::new(
+                MValue::new_set(
+                    vec![
+                        PairValue::new(MValue::Nat(8), MValue::Int(11)),
+                        PairValue::new(MValue::Nat(7), MValue::Int(11)),
+                        PairValue::new(MValue::Nat(7), MValue::Int(12)),
+                    ],
+                    MType::Pair(Box::new((MType::Nat, MType::Int))),
+                ),
+                Instruction::INIT,
+            ),
+            StackElement::new(MValue::Nat(5), Instruction::INIT),
+            StackElement::new(MValue::Int(6), Instruction::INIT),
+        ];
+        let stack_snapshots = vec![];
+        let options = RunOptions {
+            context: RunOptionsContext {
+                amount: 0,
+                sender: String::from("test_sender"),
+                source: String::from("test_source"),
+                self_address: String::from("KT1L7GvUxZH5tfa6cgZKnH6vpp2uVxnFVHKu"),
+                balance: 50_000_000,
+                level: 11,
+            },
+            pos: 0,
+        };
+
+        assert!(initial_stack.len() == 4);
+
+        match run(initial_stack, &options, stack_snapshots) {
+            Err(_) => assert!(false),
+            Ok((stack, _)) => {
+                assert!(stack.len() == 3);
+                assert_eq!(stack[0].value, MValue::Bool(true));
+                assert_eq!(stack[0].instruction, Instruction::MEM);
+            }
+        }
+    }
 }
