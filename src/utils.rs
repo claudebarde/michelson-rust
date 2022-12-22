1cbfd589a1738975531aa2c83ce1522dcd7b20b5
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2022-12-22 11:53:33 +0000

    Added pattern to catch types with 1 param to micheline_to_json

--- src/utils.rs
+++ src/utils.rs
@@ -120,8 +120,46 @@ pub fn micheline_to_json(micheline: String) -> Result<String, String> {
                 }
                 None => {
                     // checks if the string is a complex type with 1 parameter
-                    // checks if the string is a complex type with 2 parameters
-                    Err(String::from("Provided string is not 0 param type"))
+                    let type_1_param_regex = Regex::new(
+                        format!(
+                            r"^\({{0,1}}({})\s+({})*(.*)$",
+                            TYPES_1_PARAM.join("|"),
+                            ANNOT_PATTERN
+                        )
+                        .as_str(),
+                    )
+                    .unwrap();
+                    match type_1_param_regex.captures(micheline) {
+                        Some(caps) => {
+                            let main_type = caps.get(1).unwrap().as_str();
+                            let annot = match caps.get(2) {
+                                None => String::from(""),
+                                Some(annot) => format!(r#","annots":["{}"]"#, annot.as_str()),
+                            };
+                            let param_type = {
+                                let param_cap = caps.get(3).unwrap().as_str();
+                                // removes trailing parens if string started with a parens
+                                if micheline.chars().nth(0).unwrap() == '('
+                                    && param_cap.chars().nth(param_cap.len() - 1).unwrap() == ')'
+                                {
+                                    param_cap.to_string().pop();
+                                }
+
+                                param_cap
+                            };
+
+                            let param_to_json = micheline_to_json(param_type.to_string())?;
+
+                            Ok(format!(
+                                r#"{{"prim":"{}","args":[{}]{}}}"#,
+                                main_type, param_to_json, annot
+                            ))
+                        }
+                        None => {
+                            // checks if the string is a complex type with 2 parameters
+                            Err(String::from("Provided string is not 0 param type"))
+                        }
+                    }
                 }
             }
         } else {
@@ -137,7 +175,7 @@ mod test {
     use super::*;
 
     #[test]
-    pub fn test_micheline_to_json_legacy() {
+    pub fn test_micheline_to_json() {
         let simple_nat_type = String::from("nat");
         let res = micheline_to_json(simple_nat_type);
         assert!(res == Ok(String::from("{\"prim\":\"nat\"}")));
@@ -158,20 +196,52 @@ mod test {
             ))
         );
 
-        // let simple_option = String::from("option nat");
-        // let res = micheline_to_json(simple_option);
-        // assert!(
-        //     res == Ok(String::from(
-        //         "{\"prim\":\"option\",\"args\":[{\"prim\":\"nat\"}]}"
-        //     ))
-        // );
+        let simple_option = String::from("option nat");
+        let res = micheline_to_json(simple_option);
+        assert!(
+            res == Ok(String::from(
+                "{\"prim\":\"option\",\"args\":[{\"prim\":\"nat\"}]}"
+            ))
+        );
 
-        // let simple_option_with_annot = String::from("option %my_option nat");
-        // let res = micheline_to_json(simple_option_with_annot);
-        // assert!(
-        //     res == Ok(String::from(
-        //         "{\"prim\":\"option\",\"args\":[{\"prim\":\"nat\"}],\"annots\":[\"%my_option\"]}"
-        //     ))
-        // );
+        let simple_option_with_annot = String::from("option %my_option nat");
+        let res = micheline_to_json(simple_option_with_annot);
+        assert!(
+            res == Ok(String::from(
+                "{\"prim\":\"option\",\"args\":[{\"prim\":\"nat\"}],\"annots\":[\"%my_option\"]}"
+            ))
+        );
+
+        let complex_option = String::from("option (list nat)");
+        let res = micheline_to_json(complex_option);
+        assert!(
+            res == Ok(String::from(
+                "{\"prim\":\"option\",\"args\":[{\"prim\":\"list\",\"args\":[{\"prim\":\"nat\"}]}]}"
+            ))
+        );
+
+        let complex_option_with_option_annot = String::from("option %my_option (list nat)");
+        let res = micheline_to_json(complex_option_with_option_annot);
+        assert!(
+            res == Ok(String::from(
+                "{\"prim\":\"option\",\"args\":[{\"prim\":\"list\",\"args\":[{\"prim\":\"nat\"}]}],\"annots\":[\"%my_option\"]}"
+            ))
+        );
+
+        let complex_option_with_args_annot = String::from("option (list %my_list nat)");
+        let res = micheline_to_json(complex_option_with_args_annot);
+        assert!(
+            res == Ok(String::from(
+                "{\"prim\":\"option\",\"args\":[{\"prim\":\"list\",\"args\":[{\"prim\":\"nat\"}],\"annots\":[\"%my_list\"]}]}"
+            ))
+        );
+
+        let complex_option_with_both_annot = String::from("option %my_option (list %my_list nat)");
+        let res = micheline_to_json(complex_option_with_both_annot);
+        assert!(
+            res == Ok(String::from(
+                "{\"prim\":\"option\",\"args\":[{\"prim\":\"list\",\"args\":[{\"prim\":\"nat\"}],\"annots\":[\"%my_list\"]}],\"annots\":[\"%my_option\"]}"
+            ))
+        );
     }
 }
